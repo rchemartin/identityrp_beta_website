6edf696121911ea86d27169bbfd1e3933e319187
Romain Chemartin <Romain Chemartin>
2017-04-09 23:10:17 +0200

    ajout favicon, loader, modif mineures

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+$('.se-pre-con img').fadeIn(400);
+
+var cnt = 0;
+setInterval(function(){
+  cnt ==1 ? cnt=0:cnt++
+  $('.se-pre-con img').fadeOut(400);
+  $('.se-pre-con img:eq('+cnt+')').fadeIn(400)
+},0);
 $(function(){
 
-
+  setTimeout(function(){
+    $(".se-pre-con").fadeOut("slow"); }, 2000
+  );
 
   $('.bars').on('click', function(){
     $('.menu-responsive').slideToggle("slow");
@@ -12,9 +22,19 @@ $(function(){
   $('.vignette').css( 'cursor', 'pointer' );
 
 
+
+  setTimeout(function(){
+    $('.btn-rejoindre').slideDown("slow");
+    $('.banner-content').slideDown("slow");
+  },2000);
+
+
+
+
   $('.goToDevblog').on('click', function(){
     window.location.href = 'https://forum.identityrp.fr/viewforum.php?f=4&sid=f2d297b618593ac98115533cf3423d1b';
   })
+
 
 
 });
